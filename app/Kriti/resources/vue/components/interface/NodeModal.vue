035340dda21d0f6e78f9a09f0162578703b0d6cc
Alex Blaze <zen@8ber.ru>
2024-03-09 03:58:23 +0400

    Установил шрифт Roboto

--- app/Kriti/resources/vue/components/interface/NodeModal.vue
+++ app/Kriti/resources/vue/components/interface/NodeModal.vue
@@ -1,6 +1,8 @@
 <template>
     <div v-if="node_data" class="node-modal">
-        Модальное окно нода {{ node_data }}
+        <div class="node-modal__body">
+            Модальное окно нода {{ node_data }}
+        </div>
     </div>
 </template>
 
@@ -28,6 +30,23 @@ export default {
 
 <style lang="scss">
 .node-modal {
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: #0000009c;
 
+    &__body {
+        background: #fff;
+        width: 80%;
+        min-height: 300px;
+        margin-top: 30px;
+        padding: 15px;
+        border-radius: 10px;
+    }
 }
 </style>
