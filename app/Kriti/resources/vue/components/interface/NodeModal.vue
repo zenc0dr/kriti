3308ff47145632fab20109ab24eeb7edf032375d
Alex Blaze <zen@8ber.ru>
2024-03-09 04:07:27 +0400

    Немного стилизовал модалку нода

--- app/Kriti/resources/vue/components/interface/NodeModal.vue
+++ app/Kriti/resources/vue/components/interface/NodeModal.vue
@@ -1,7 +1,17 @@
 <template>
     <div v-if="node_data" class="node-modal">
         <div class="node-modal__body">
-            Модальное окно нода {{ node_data }}
+            <div class="node-modal__header">
+                <div class="node-modal__title">
+                    Тут заголовок модуля {{ node_data }}
+                </div>
+                <div class="node-modal__close">
+                    <i class="bi bi-x-square-fill"></i>
+                </div>
+            </div>
+            <div class="node-modal__content">
+
+            </div>
         </div>
     </div>
 </template>
@@ -47,6 +57,24 @@ export default {
         margin-top: 30px;
         padding: 15px;
         border-radius: 10px;
+    }
+
+    &__header {
+        display: flex;
+    }
+
+    &__title {
+
+    }
+
+    &__close {
+        i {
+            
+        }
+    }
+
+    &__content {
+
     }
 }
 </style>
